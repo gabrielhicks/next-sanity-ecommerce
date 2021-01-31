edf893065c53018240ca6200037ec4f7ee77c58b
gabrielhicks <gabrielhicks@mail.com>
2021-01-31 17:59:20 -0500

    added hero 2

--- components/sections/index.js
+++ components/sections/index.js
@@ -1,3 +1,4 @@
 export { default as Hero } from "./Hero";
+export { default as Hero2 } from "./Hero2";
 export { default as TextSection } from "./TextSection";
 export { default as ImageSection } from "./ImageSection";
